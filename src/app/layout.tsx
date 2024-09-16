f51a86aaff891a83eb488062176f4d03beb38525
f0rest775 <f0rest15@hotmail.com>
2024-09-16 15:21:07 -0300

    update to domain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,6 +23,36 @@ export default function RootLayout({
         <meta name="viewport"
           content="width=device-width, initial-scale=1.0 ,maximum-scale=1.0 , user-scalable=no"
         />
+
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '986429856352502');
+          fbq('track', 'PageView');
+        `}
+        </Script>
+
+
+        <Script
+          src="//widget.manychat.com/1369769_f6b1f.js"
+          strategy="afterInteractive"
+          defer
+        />
+
+        <Script
+          src="https://mccdn.me/assets/js/widget.js"
+          strategy="afterInteractive"
+          defer
+        />
+
+
       </head>
       <body
         className={`${inter.className} antialiased`}
