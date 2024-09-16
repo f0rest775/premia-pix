888b708c5de3f748249c68c134af0711a4690781
f0rest775 <f0rest15@hotmail.com>
2024-09-16 15:34:12 -0300

    update to domain

--- src/functions/urls.ts
+++ src/functions/urls.ts
@@ -1,7 +1,6 @@
 const urls = [
   "https://go.perfectpay.com.br/PPU38COPTO0?",
-  "https://checkout.perfectpay.com.br/pay/PPU38CP1ULO?"
-
+  //"https://checkout.perfectpay.com.br/pay/PPU38CP1ULO?"
 ];
 
 export const CHECKOUT_URL = urls[Math.floor(Math.random() * urls.length)];
